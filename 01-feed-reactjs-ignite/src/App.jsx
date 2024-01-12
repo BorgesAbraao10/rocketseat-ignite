87cb6023a9675819086fb17af11024f36b4fa9c4
Abraão Borges <abraao10clear@gmail.com>
2024-01-12 15:19:15 -0300

    Last update

--- 01-feed-reactjs-ignite/src/App.jsx
+++ 01-feed-reactjs-ignite/src/App.jsx
@@ -1,6 +1,16 @@
+import { Post } from "./Post";
 
-function App() {
-  return <h1>Hello World</h1>;
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Abraão Borges"
+        content="Olá! Sou um entusiasta da tecnologia em transição de carreira, apaixonado por programação e pela arte de transformar problemas complexos em soluções criativas. Cada linha de código que escrevo é um passo em direção a um futuro onde a inovação e a resolução de desafios andam de mãos dadas. Estou ansioso para aplicar meu conhecimento e criar um impacto positivo por meio da tecnologia.👨‍💻"
+      />
+      <Post
+        author="Ana Borges"
+        content="Sou uma estudante de programação apaixonado por criar soluções para problemas complexos usando tecnologia."
+      />
+    </div>
+  );
 }
-
-export default App;
