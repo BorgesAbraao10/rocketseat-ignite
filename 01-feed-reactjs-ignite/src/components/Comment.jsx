6fe52cf6260117fca98d26dd81d789a772f170b1
Abraão Borges <abraao10clear@gmail.com>
2024-01-18 10:33:01 -0300

    Last update

---
+++ 01-feed-reactjs-ignite/src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.Comment}>
+      <img src="https://github.com/BorgesAbraao10.png" alt="Imagem Avatar" />
+
+      <div className={styles.comment.Box}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Abraão Borges</strong>
+              <time
+                title="16 de Janeiro às 19:04"
+                dateTime="2024-01-16 19:04:01"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Vamos Tricolor!!!!!!!!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
